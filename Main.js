174703a12d2eebf464d126ac56d85254d03a436b
jayanth-chepuri <jayanthnagagandhi_chepuri@srmap.edu.in>
2026-04-07 16:25:42 +0530

    haha

--- Main.js
+++ Main.js
@@ -4,6 +4,7 @@ let sum = a + b;
 let difference = a - b;
 let product = a * b;
 let quotient = a / b;
+console.log("The sum of " + a + " and " + b + " is: " + sum);
 console.log("The difference of " + a + " and " + b + " is: " + difference);
 console.log("The product of " + a + " and " + b + " is: " + product);
 console.log("The quotient of " + a + " and " + b + " is: " + quotient);
