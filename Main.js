87d4d02ea816b561ab8b0079314c737f05369220
jayanth-chepuri <jayanthnagagandhi_chepuri@srmap.edu.in>
2026-04-07 15:24:00 +0530

    Third commit

--- Main.js
+++ Main.js
@@ -1,5 +1,11 @@
 let a = 5;
 let b = 10;
 let sum = a + b;
+let difference = a - b;
+let product = a * b;
+let quotient = a / b;
+console.log("The difference of " + a + " and " + b + " is: " + difference);
+console.log("The product of " + a + " and " + b + " is: " + product);
+console.log("The quotient of " + a + " and " + b + " is: " + quotient);
 console.log("The sum of " + a + " and " + b + " is: " + sum);
 console.log("Hello, World!");
